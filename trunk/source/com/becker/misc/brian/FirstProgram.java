b87a8a45c0928d0120acfc71c95c0c28cca9a12b
becker <becker@5fdb6067-0ee0-0310-88fc-90c9ac7ca001>
2005-05-22 14:10:43 +0000

    add experimental program for Brian. fix number formatting for large numbers

--- trunk/source/com/becker/misc/brian/FirstProgram.java
+++ trunk/source/com/becker/misc/brian/FirstProgram.java
@@ -13,14 +13,24 @@ public final class FirstProgram
 
     public static void main( String[] args )
     {
-        int count = 0;
+
+        int count = 1;
 
         do {
 
-            System.out.print("Brian ");
+
+            System.out.print("Brian ("+count+")  ");
+
             count = count + 1;
 
+            if (count % 25 == 0) {
+                System.out.println();
+            }
 
-        }  while (count < 100);
+
+        }  while (count <= 10000);
+
+        System.out.println("DONE!");
+
     }
 }
