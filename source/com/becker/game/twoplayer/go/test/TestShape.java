b17cc2437f277a6be84b2c4cb9f1c73379c59ff6
Barry Becker <barrybecker4@yahoo.com>
2005-06-18 16:57:04 +0000

    add Benson tests. add tests for shape. add simple programs for Brian.

---
+++ source/com/becker/game/twoplayer/go/test/TestShape.java
@@ -0,0 +1,48 @@
+package com.becker.game.twoplayer.go.test;
+
+import junit.framework.Assert;
+import junit.framework.TestSuite;
+import junit.framework.Test;
+import com.becker.game.twoplayer.go.GoBoard;
+import com.becker.game.twoplayer.go.GoBoardPosition;
+import com.becker.game.common.BoardPosition;
+
+
+/**
+ * @author Barry Becker
+ */
+public class TestShape extends GoTestCase {
+
+
+    public void testShape1() {
+        restore("problem_shape1");
+
+        checkShape(4, 4, 0);
+        checkShape(9, 4, 0);
+
+        checkShape(10, 8, 1);
+        checkShape(11, 8, 1);
+        checkShape(11, 9, 1);
+
+        checkShape(4, 9, 3);
+        checkShape(5, 9, 6);
+        checkShape(5, 10, 6);
+        checkShape(6, 10, 3);
+
+    }
+
+
+
+    private void checkShape(int r, int c, int expectedShapeScore) {
+        GoBoard board = (GoBoard)controller_.getBoard();
+        GoBoardPosition position = (GoBoardPosition)board.getPosition(r, c);
+        int n = board.formsBadShape(position);
+        Assert.assertTrue("Expected "+expectedShapeScore+" but got "+n+" for "+position, n == expectedShapeScore);
+    }
+
+
+
+    public static Test suite() {
+        return new TestSuite(TestShape.class);
+    }
+}
