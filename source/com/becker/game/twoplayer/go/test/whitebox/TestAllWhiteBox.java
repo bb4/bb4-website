43d257754513910d91990f9c8f8d0c814fe74fca
Barry Becker <barrybecker4@yahoo.com>
2005-07-16 15:05:01 +0000

    start adding whitebox testcases for go.

---
+++ source/com/becker/game/twoplayer/go/test/whitebox/TestAllWhiteBox.java
@@ -0,0 +1,29 @@
+package com.becker.game.twoplayer.go.test.whitebox;
+
+import com.becker.game.twoplayer.go.test.GoTestCase;
+import com.becker.game.twoplayer.go.test.TestBlunderCollection;
+import com.becker.game.twoplayer.go.test.TestEscapeCaptureCollection;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Test out all of my testable methods in my go related classes.
+ *
+ * @author Barry Becker
+ */
+public class TestAllWhiteBox extends GoTestCase {
+
+
+    /**
+     * @return all the junit test caes to run (in this class)
+     */
+    public static Test suite() {
+
+        TestSuite suite =  new TestSuite("Whitebox Tests");
+
+        suite.addTestSuite(TestGoBoard.class);
+        suite.addTestSuite(TestGoGroup.class);
+
+        return suite;
+    }
+}
---
+++ source/com/becker/game/twoplayer/go/test/whitebox/TestGoGroup.java
@@ -0,0 +1,11 @@
+package com.becker.game.twoplayer.go.test.whitebox;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: becker
+ * Date: Jul 16, 2005
+ * Time: 6:23:53 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestGoGroup {
+}
