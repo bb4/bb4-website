056498ba912fabb3ad79f82745b57bc920bbab04
Barry Becker <barrybecker4@yahoo.com>
2005-10-01 11:14:15 +0000

    del old classes. add new ones.

---
+++ source/com/becker/game/twoplayer/common/TwoPlayerOptions.java
@@ -0,0 +1,223 @@
+package com.becker.game.twoplayer.common;
+
+import com.becker.game.twoplayer.common.search.SearchStrategy;
+import com.becker.game.common.GameContext;
+
+/**
+ * Encapsulate two player options here to keep the TwoPlayerController class mush simpler.
+ *
+ * @author Barry Becker
+ * Date: Aug 20, 2005
+ */
+public class TwoPlayerOptions {
+
+
+    // if true then use alpha beta pruning
+    private static final boolean ALPHA_BETA = true;
+    // if true then use quiescent search
+    private static final boolean QUIESCENCE = false;
+
+    private boolean alphaBeta_ = ALPHA_BETA;
+    private boolean quiescence_ = QUIESCENCE;
+
+    // if true then try to show a dialog visualizing the game tree.
+    private boolean showGameTree_ = false;
+    // if true then show all the moves the computer is considering when it considers them.
+    private boolean showComputerAnimation_ = false;
+
+    private int strategyMethod_ = SearchStrategy.MINIMAX;  // the defualt search method.
+    private int lookAhead_;
+    private int bestPercentage_;
+
+    private boolean autoOptimize_;
+    private String autoOptimizeFile_ = null;
+
+    private String preferredTone_ = null;
+
+    private String player1Name_ = GameContext.getLabel("PLAYER1");
+    private String player2Name_ = GameContext.getLabel("PLAYER2");
+
+    /**
+     *
+     * @param defaultLookAhead default number of moves to look ahead.
+     * @param defaultBestPercentage default number of best moves to consider at each ply.
+     */
+    public TwoPlayerOptions(int defaultLookAhead, int defaultBestPercentage, String preferredTone) {
+        lookAhead_ = defaultLookAhead;
+        bestPercentage_ = defaultBestPercentage;
+        preferredTone_ = preferredTone;
+    }
+
+
+    /**
+     * @return the strategy method currently being used.
+     */
+    public int getSearchStrategyMethod()
+    {
+        return strategyMethod_;
+    }
+
+    /**
+     * @param method the desired search strategy for evaluating the game tree.
+     * (eg MINIMAX, NEGAMAX, etc)
+     */
+    public final void setSearchStrategyMethod( int method )
+    {
+        strategyMethod_ = method;
+    }
+
+
+    /**
+     * @return the amount of lookahead (number of plys) used by the search strategy
+     */
+    public final int getLookAhead()
+    {
+        return lookAhead_;
+    }
+
+    /**
+     * @param look the number of plys to look ahaead.
+     */
+    public final void setLookAhead( int look )
+    {
+        lookAhead_ = look;
+    }
+
+    /**
+     * @return  the percentage of top moves considered at each ply
+     */
+    public final int getPercentageBestMoves()
+    {
+        return bestPercentage_;
+    }
+
+    /**
+     * @param bestPercentage  the percentage of top moves considered at each ply
+     */
+    public final void setPercentageBestMoves( int bestPercentage )
+    {
+        bestPercentage_ = bestPercentage;
+    }
+
+
+    /**
+     * @return true if alpha-beta pruning is being employed by the search strategy.
+     */
+    public final boolean getAlphaBeta()
+    {
+        return alphaBeta_;
+    }
+
+    /**
+     * @param ab set whether of not to use alpha-beta pruning
+     */
+    public final void setAlphaBeta( boolean ab )
+    {
+        alphaBeta_ = ab;
+    }
+
+    /**
+     * @return whether or not the quiescent search option is being used by the search strategy
+     */
+    public final boolean getQuiescence()
+    {
+        return quiescence_;
+    }
+
+    public final void setQuiescence( boolean quiescence )
+    {
+        quiescence_ = quiescence;
+    }
+
+    /**
+     * @return whether or not we are showing the game tree (primarily used for debugging)
+     */
+    public final boolean getShowGameTree()
+    {
+        return showGameTree_;
+    }
+
+    public final void setShowGameTree( boolean show )
+    {
+        showGameTree_ = show;
+    }
+
+    /**
+     * @return whether we are showing an animated representation of the computers thought process for each move
+     * (takes a long time)
+     */
+    public final boolean getShowComputerAnimation()
+    {
+        return showComputerAnimation_;
+    }
+
+    public final void setShowComputerAnimation( boolean computerAnimation )
+    {
+        showComputerAnimation_ = computerAnimation;
+    }
+
+    /**
+     * Optimize the evaluation weights by running many games where the computer
+     * plays against itself.
+     * @param autoOptimize whether or not to do an optimization run when you press ok.
+     */
+    public final void setAutoOptimize(boolean autoOptimize)
+    {
+        autoOptimize_ = autoOptimize;
+    }
+
+
+    /**
+     * @return true it the controller is set to auto optimize instead of play a regular game
+     */
+    public boolean isAutoOptimize()
+    {
+        return autoOptimize_;
+    }
+
+    /**
+     * @param autoOptimizeFile the log file to write to when autp optimizing.
+     */
+    public final void setAutoOptimizeFile(String autoOptimizeFile)
+    {
+        autoOptimizeFile_ = autoOptimizeFile;
+    }
+
+    /**
+     * @return the log file to write to when autp optimizing.
+     */
+    public final String getAutoOptimizeFile()
+    {
+        assert (autoOptimizeFile_!=null) : "There is no auto optimize file";
+        return autoOptimizeFile_;
+    }
+
+
+    public String getPreferredTone() {
+        return preferredTone_;
+    }
+
+    public void setPlayerName(boolean player1, String name)  {
+         if (player1) {
+            player1Name_ = name;
+        }
+        else {
+            player2Name_ = name;
+        }
+    }
+
+    /**
+     * @param player1
+     * @return player 1's name if pplayer 1 is true else p2's name
+     */
+    public String getPlayerName(boolean player1)
+    {
+        if (player1) {
+            return player1Name_;
+        }
+        else {
+            return player2Name_;
+        }
+    }
+
+}
---
+++ source/com/becker/game/twoplayer/go/GoGroupUtil.java
@@ -0,0 +1,195 @@
+package com.becker.game.twoplayer.go;
+
+import com.becker.game.common.*;
+import com.becker.common.*;
+
+import java.util.*;
+
+/**
+ * static methods for determing properties of groups
+ * Includes Bensons algoritm for unconditional life
+ * @author Barry Becker Date: Aug 28, 2005
+ */
+public final class GoGroupUtil {
+
+
+     private GoGroupUtil() {};
+
+    /**
+     * Use Benson's algorithm (1977) to determine if a set of strings and eyes within a group
+     * is unconditionally alive
+     * @return true if unconditionally alive
+     */
+    public static boolean isUnconditionallyAlive(GoGroup group, GoBoard board) {
+
+        // mark all the strings in the group as not UA
+        Set candidateStrings = new HashSet();
+        for (Object s : group.getMembers()) {
+            GoString str = (GoString)s;
+            str.setUnconditionallyAlive(true);
+            candidateStrings.add(str);
+        }
+
+        findNeighborStringSets(group, board);
+
+        // now create the neighbor eye sets for each qualified string
+        for (Object e : group.getEyes()) {
+            GoEye eye = (GoEye) e;
+            if (eye.getNbrs() != null) {
+                for (Object s : eye.getNbrs()) {
+                    GoString str = (GoString) s;
+                    if (str.getNbrs() == null) {
+                        str.setNbrs(new HashSet());
+                    }
+                    // only add the eye if every unoccupied position in the eye is adjacent to the str
+                    if  (eye.allUnocupiedAdjacentToString(str, board)) {
+                        str.getNbrs().add(eye);
+                    }
+                }
+            }
+        }
+
+        boolean done;
+        do {
+            done = true;
+            for (Object e : group.getEyes()) {
+                GoEye eye = (GoEye)e;
+                eye.setUnconditionallyAlive(true);
+                if (eye.getNbrs() != null) {
+                    for (Object str : eye.getNbrs()) {
+                        GoString nbrStr = (GoString) str;
+                        if (!nbrStr.isUnconditionallyAlive()) {
+                            eye.setUnconditionallyAlive(false);
+                        }
+                    }
+                }
+            }
+            Iterator it = candidateStrings.iterator();
+            while (it.hasNext()) {
+                GoString str = (GoString) it.next();
+                // find the number of ua eyes adjacent
+                int numUAEyesAdjacent = 0;
+                if (str.getNbrs() != null) {
+                    for (Object e : str.getNbrs()) {
+                        GoEye eye = (GoEye) e;
+                        if (eye.isUnconditionallyAlive()) {
+                            numUAEyesAdjacent++;
+                        }
+                    }
+                }
+                if (numUAEyesAdjacent < 2) {
+                    str.setUnconditionallyAlive(false);
+                    it.remove();
+                    done = false; // something changed
+                }
+            }
+
+        }  while ( !(done || candidateStrings.isEmpty()));
+
+        // clear str nbrs
+        for (Object s : group.getMembers()) {
+            GoString str = (GoString)s;
+            str.setNbrs(null);
+        }
+
+       return  !candidateStrings.isEmpty();
+    }
+
+
+    private static void findNeighborStringSets(GoGroup group, GoBoard board) {
+        // first find the neighbor string sets for each true eye in the group
+        Set candidateUAStrings = new HashSet();
+        for (Object e : group.getEyes()) {
+            GoEye eye = (GoEye) e;
+            if (eye.getNbrs() == null) {
+                eye.setNbrs(new HashSet());
+            }
+            for (Object point : eye.getMembers()) {
+                GoBoardPosition pos = (GoBoardPosition) point;
+                if (pos.isUnoccupied()) {
+                    Set nbrs = board.getNobiNeighbors(pos, eye.isOwnedByPlayer1(), NeighborType.FRIEND);
+                    for (Object n : nbrs) {
+                        GoBoardPosition nbr = (GoBoardPosition) n;
+                        if (nbr.getString().getGroup() != group) {
+                            // this eye is not UA.
+                            eye.setNbrs(null);
+                            break;
+                        }
+                        else {
+                            if (eye.getNbrs() != null ) {
+                                eye.getNbrs().add(nbr.getString());
+                                candidateUAStrings.add(nbr.getString());
+                            }
+                        }
+                    }
+                }
+            }
+            GameContext.log(2, "num string nbrs of eyes = "
+                    + ((eye.getNbrs() == null)? 0 : eye.getNbrs().size()));
+        }
+    }
+
+
+
+    /////////// utility/debugging methods  /////////////////////////
+
+
+    /** see if the group contains all the stones that are in the specified list (it may contain others as well)
+     * @param stones list of stones to check if same as those in this group
+     * @return true if all the strings are in this group
+     */
+    public static  boolean contains(GoGroup group, List stones )
+    {
+        Iterator it = stones.iterator();
+        while ( it.hasNext() ) {
+            GoString s = ((GoBoardPosition) it.next()).getString();
+            if ( !group.getMembers().contains( s ) )
+                return false;
+        }
+        return true;
+    }
+
+    /**
+     * @param stones list of stones to check if same as those in this group
+     * @return true if this group exacly contains the list of stones and no others
+     */
+    public static boolean exactlyContains(GoGroup group, List stones)
+    {
+        if ( !contains(group, stones ) )
+            return false;
+        // make sure that every stone in the group is also in the list.
+        // that way we are assured that they are the same.
+        Iterator sIt = group.getStones().iterator();
+        while ( sIt.hasNext() ) {
+            GoBoardPosition s = (GoBoardPosition) sIt.next();
+            if ( !stones.contains( s ) )
+                return false;
+        }
+        return true;
+    }
+
+    /**
+     * go through the groups strings and verify that they are valid (have all nobi connections)
+     */
+    public static void confirmValidStrings(GoGroup group, GoBoard b )
+    {
+        Iterator it = group.getMembers().iterator();
+        while ( it.hasNext() ) {
+            GoString string = (GoString) it.next();
+            string.confirmValid( b );
+        }
+    }
+
+    public static void confirmNoNullMembers(GoGroup group)
+    {
+        Iterator it = group.getStones().iterator();
+        boolean failed = false;
+        while (it.hasNext()) {
+            GoBoardPosition s = (GoBoardPosition)it.next();
+            if (s.getPiece()==null) failed = true;
+        }
+        if (failed) {
+            Assert.exception("Group contains an empty position: "+group.toString());
+        }
+    }
+}
