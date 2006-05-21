6f5be0c5c50e49c1996fcbc024783db3af994b8c
becker <becker@5fdb6067-0ee0-0310-88fc-90c9ac7ca001>
2006-05-21 16:53:34 +0000

    suport ofr online play.

---
+++ trunk/source/com/becker/game/multiplayer/poker/ui/PokerOnlineGamesTable.java
@@ -0,0 +1,50 @@
+package com.becker.game.multiplayer.poker.ui;
+
+import com.becker.game.common.online.*;
+import com.becker.game.multiplayer.common.ui.*;
+import com.becker.game.multiplayer.poker.*;
+import com.becker.game.multiplayer.poker.online.*;
+
+import java.awt.*;
+import java.util.*;
+
+/**
+ * @@ need to have a column for chiper per player, and bet limit.
+ *
+ * @author Barry Becker Date: May 13, 2006
+ */
+ public class PokerOnlineGamesTable extends MultiPlayerOnlineGamesTable {
+
+    private static final Random RANDOM = new Random();
+
+    public PokerOnlineGamesTable(OnlineGameTable[] tables) {
+         super(tables);
+    }
+
+    protected void addRow(OnlineGameTable onlineTable) {
+        Object d[] = new Object[getNumColumns()];
+        d[JOIN_INDEX] = true; // true if active player is in this table
+        d[TABLE_NAME_INDEX] = onlineTable.getName();
+        d[PLAYER_NAMES_INDEX] = onlineTable.getPlayerNames();
+        //d[CASH_INDEX] = DEFAULT_CASH_AMOUNT; //p.getCash();
+
+        getModel().addRow(d);
+
+        selectedTable_ = onlineTable;
+    }
+
+
+    public OnlineGameTable createOnlineTable(String initialPlayerName) {
+        // we should pass in a strucutre that contains the initial game options.
+        PokerPlayer player = new PokerHumanPlayer(initialPlayerName, 100, getRandomColor());
+        OnlinePokerTable gameTable = new OnlinePokerTable(getUniqueName(), player);
+        return gameTable;
+    }
+
+    private static Color getRandomColor() {
+
+        int r = RANDOM.nextInt(256);
+        return new Color(r, 255 -r, RANDOM.nextInt(256));
+    }
+
+}
