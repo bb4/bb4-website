9e61960eb75a3e2bd913220cfc5eecfc9eea574a
Barry Becker <barrybecker4@yahoo.com>
2006-05-21 16:53:34 +0000

    suport ofr online play.

---
+++ source/com/becker/game/multiplayer/poker/ui/OnlinePokerDialog.java
@@ -0,0 +1,38 @@
+package com.becker.game.multiplayer.poker.ui;
+
+import com.becker.game.common.*;
+import com.becker.game.common.online.*;
+import com.becker.game.common.ui.*;
+import com.becker.game.multiplayer.common.ui.*;
+import com.becker.game.multiplayer.poker.online.*;
+
+import java.awt.*;
+
+/**
+ * Manage online player games.
+ * Player can join no more than one table at a time.
+ *
+ * @author Barry Becker Date: May 14, 2006
+ */
+public class OnlinePokerDialog extends MultiPlayerOnlineGameDialog {
+
+    public OnlinePokerDialog(Frame parent, ViewerCallbackInterface viewer) {
+        super(parent, viewer);
+    }
+
+    protected MultiPlayerOnlineGamesTable createOnlineGamesTable(String playersName) {
+        return new PokerOnlineGamesTable(null);
+    }
+
+    /**
+     * You are free to set your own options for the table that you are creating.
+     */
+    protected GameOptionsDialog createNewGameTableDialog() {
+        return new PokerOptionsDialog(null, controller_);
+    }
+
+    protected ServerConnection createServerConnection() {
+         return new ServerConnection(OnlinePokerServer.PORT);
+     }
+
+}
