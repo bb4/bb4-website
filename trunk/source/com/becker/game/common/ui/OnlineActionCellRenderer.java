116c8cc6bbcb6c1a710f277e09e7f949a534b595
becker <becker@5fdb6067-0ee0-0310-88fc-90c9ac7ca001>
2006-05-21 16:52:52 +0000

    add the beginnings of online play support for poker.

---
+++ trunk/source/com/becker/game/common/ui/OnlineActionCellRenderer.java
@@ -0,0 +1,77 @@
+package com.becker.game.common.ui;
+
+import com.becker.game.common.*;
+
+import javax.swing.*;
+import javax.swing.table.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ * Renders the action button in the OnlineGamesTable.
+ *
+ * @author Barry Becker Date: May 20, 2006
+ */
+public class OnlineActionCellRenderer implements TableCellRenderer {
+
+    private JoinButton joinButton_;
+
+
+
+    public OnlineActionCellRenderer(ActionListener listener)
+    {
+        joinButton_ = new JoinButton(listener);
+
+    }
+
+    public Component getTableCellRendererComponent(JTable table,
+                                                   Object value,   // boolean
+                                                   boolean isSelected, boolean hasFocus,
+                                                   int row, int col)
+    {
+        joinButton_.setEnabled((Boolean) value);
+
+        joinButton_.setRow(row);
+        return joinButton_;
+    }
+
+
+    public int getRow() {
+        return joinButton_.getRow();
+    }
+
+    public void addMouseListener(MouseListener l) {
+        joinButton_.addMouseListener(l);
+    }
+
+
+
+    public static class JoinButton extends JButton {
+
+        private int row_;
+
+        public JoinButton(ActionListener listener) {
+
+            super(GameContext.getLabel("JOIN"));
+            setToolTipText(GameContext.getLabel("JOIN_TIP"));
+            setHorizontalAlignment(JLabel.CENTER);
+            setPreferredSize(new Dimension(100, 18));
+            addActionListener(listener);
+        }
+
+
+        public int getRow() {
+            return row_;
+        }
+
+        public void setRow(int row) {
+            row_ = row;
+        }
+
+        public String toString() {
+            return "JoinButton row = "+getRow() ;
+        }
+    }
+
+}
+
---
+++ trunk/source/com/becker/game/multiplayer/poker/online/OnlinePokerTable.java
@@ -0,0 +1,16 @@
+package com.becker.game.multiplayer.poker.online;
+
+import com.becker.game.common.*;
+import com.becker.game.common.online.*;
+
+/**
+ * @author Barry Becker Date: May 13, 2006
+ */
+public class OnlinePokerTable extends OnlineGameTable {
+
+    public OnlinePokerTable(String name, Player initialPlayer) {
+        super(name, initialPlayer);
+    }
+
+
+}
