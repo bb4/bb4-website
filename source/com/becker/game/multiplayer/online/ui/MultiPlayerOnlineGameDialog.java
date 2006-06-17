dafc055cdfc36a7ce7dc13763a59c29b549d88ed
Barry Becker <barrybecker4@yahoo.com>
2006-06-17 15:41:33 +0000

    some rearranging.

---
+++ source/com/becker/game/multiplayer/online/ui/MultiPlayerOnlineGameDialog.java
@@ -0,0 +1,145 @@
+package com.becker.game.multiplayer.online.ui;
+
+import com.becker.game.common.*;
+import com.becker.game.common.ui.*;
+import com.becker.game.online.*;
+import com.becker.game.online.ui.*;
+import com.becker.ui.*;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ * Manage multiplayer online game tables.
+ *
+ * @author Barry Becker Date: May 14, 2006
+ */
+public abstract class MultiPlayerOnlineGameDialog extends OnlineGameDialog
+                                                  implements ActionListener {
+
+    // new table in play online tab
+    private JTextField localPlayerName_;
+    private GradientButton createTableButton_;
+    private MultiPlayerOnlineGamesTable onlineGamesTable_;
+    private GameOptionsDialog createGameTableDialog_;
+
+    /**
+     * Constructor
+     * @param parent
+     * @param viewer
+     */
+    public  MultiPlayerOnlineGameDialog(Frame parent, ViewerCallbackInterface viewer) {
+        super(parent, viewer);
+    }
+
+
+    /**
+     *  There is a button for creating a new online table at the top.
+     *
+     * The play online table as a row for each virtual table that an online player can join.
+     * There is a join button next to each row in the table.
+     * The player must join exactly one table before the start button gets enabled.
+     * Join button, changes to "leave", after joining a table.
+     * If you join a different table, you leave the last one that you joined.
+     * A table (row) is removed if everyone leaves it.
+     */
+    protected JPanel createPlayOnlinePanel()
+    {
+        createGameTableDialog_ = createNewGameTableDialog();
+
+        JPanel playOnlinePanel = new JPanel(new BorderLayout());
+        playOnlinePanel.setBorder(
+                BorderFactory.createTitledBorder( BorderFactory.createEtchedBorder(),
+                                                  "Join a table to play others online") );
+
+        JPanel headerPanel = new JPanel(new BorderLayout());
+        JLabel titleLabel = new JLabel(GameContext.getLabel("ONLINE_TABLES"));
+        JPanel buttonsPanel = new JPanel(new BorderLayout());
+
+        createTableButton_ = new GradientButton(GameContext.getLabel("CREATE_TABLE"));
+        createTableButton_.setToolTipText( GameContext.getLabel("CREATE_TABLE_TIP") );
+        createTableButton_.addActionListener(this);
+        buttonsPanel.add(createTableButton_, BorderLayout.EAST);
+
+        JPanel namePanel = new JPanel(new BorderLayout());
+        JLabel nameLabel = new JLabel("Your Name: ");
+        localPlayerName_ = new JTextField("<name>");
+        //localPlayerName_.setPreferredSize(new Dimension(180, 14));
+        namePanel.add(nameLabel, BorderLayout.WEST);
+        namePanel.add(localPlayerName_, BorderLayout.CENTER);
+        JPanel fill = new JPanel();
+        fill.setPreferredSize(new Dimension(180, 20));
+        namePanel.add(fill, BorderLayout.EAST);
+
+        headerPanel.add(namePanel, BorderLayout.NORTH);
+        headerPanel.add(titleLabel, BorderLayout.CENTER);
+        headerPanel.add(buttonsPanel, BorderLayout.EAST);
+        playOnlinePanel.add(headerPanel, BorderLayout.NORTH);
+
+        onlineGamesTable_ = createOnlineGamesTable(localPlayerName_.getText());
+
+        playOnlinePanel.setPreferredSize( new Dimension(500, 300) );
+        playOnlinePanel.add( new JScrollPane(onlineGamesTable_.getTable()) , BorderLayout.CENTER );
+
+        return playOnlinePanel;
+    }
+
+
+    protected abstract MultiPlayerOnlineGamesTable createOnlineGamesTable(String name);
+
+    /**
+     * You are free to set your own options for the table that you are creating.
+     */
+    protected abstract GameOptionsDialog createNewGameTableDialog();
+
+    /**
+     * An online table has filled with players and is ready to start.
+     */
+    protected void startGame()
+    {
+        controller_.setPlayers(onlineGamesTable_.getSelectedTable().getPlayers());
+    }
+
+    public void handleServerUpdate(GameCommand cmd) {
+
+        System.out.println("got an update of the multiplayer table list from the server:\n" + cmd);
+
+        switch (cmd.getName())  {
+            case UPDATE_TABLES :
+                OnlineGameTableList tableList = (OnlineGameTableList) cmd.getArgument();
+                System.out.println("There were "+ tableList.size()+" tables returned from server.");
+
+                onlineGamesTable_.removeAllRows();
+
+                for (int i=0; i<tableList.size(); i++) {
+                    onlineGamesTable_.addRow(tableList.get(i));
+                }
+                break;
+           case CHANGE_NAME :
+                break;
+           default : assert false : "Unexpected command name :"+ cmd.getName();
+        }
+
+    }
+
+    /**
+     * The user has done something to change the table list (e.g. add a new game table).
+     * @param e
+     */
+    public void actionPerformed( ActionEvent e )
+    {
+        //super.actionPerformed(e);
+        Object source = e.getSource();
+
+        if (source == createTableButton_) {
+            createGameTableDialog_.showDialog();
+            OnlineGameTable newTable = onlineGamesTable_.createOnlineTable(localPlayerName_.getText());
+
+            // now add it to this list as a new row and tell the server to add it.
+            //onlineGamesTable_.addRow(newTable);
+            serverConnection_.addGameTable(newTable);
+        }
+    }
+
+}
---
+++ source/com/becker/game/multiplayer/online/ui/MultiPlayerOnlineGamesTable.java
@@ -0,0 +1,149 @@
+package com.becker.game.multiplayer.online.ui;
+
+import com.becker.game.common.*;
+import com.becker.game.multiplayer.common.ui.*;
+import com.becker.game.online.*;
+import com.becker.game.online.ui.*;
+
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.table.*;
+import java.awt.event.*;
+
+/**
+ * A table that has a row for each virtual online game table.
+ *
+ * @author Barry Becker
+ */
+public abstract class MultiPlayerOnlineGamesTable implements ActionListener {
+
+
+    protected JTable table_;
+
+    protected static final int JOIN_INDEX = 0;
+    protected static final int TABLE_NAME_INDEX = 1;
+    protected static final int PLAYER_NAMES_INDEX = 2;
+
+    protected static final String ACTION = GameContext.getLabel("ACTION");
+    protected static final String TABLE_NAME = GameContext.getLabel("TABLE_NAME");
+    protected static final String PLAYER_NAMES = GameContext.getLabel("PLAYER_NAMES");
+
+    protected OnlineGameTable selectedTable_;
+
+    private static final String[] COLUMN_NAMES = {ACTION, TABLE_NAME, PLAYER_NAMES };
+
+    private static int counter_;
+
+    /**
+     * constructor
+     */
+    public MultiPlayerOnlineGamesTable(OnlineGameTable[] tables)
+    {
+        selectedTable_ = null;
+
+        initializeTable(tables);
+    }
+
+    protected void initializeTable(OnlineGameTable[] tables)
+    {
+        TableModel m = new PlayerTableModel(COLUMN_NAMES, 0, true);
+        table_ = new JTable(m);
+
+        TableColumn nameColumn = table_.getColumn(TABLE_NAME);
+        nameColumn.setPreferredWidth(130);
+
+        TableColumn actionColumn = table_.getColumn(ACTION);
+        actionColumn.setCellRenderer(new OnlineActionCellRenderer(this));
+        actionColumn.setCellEditor(new OnlineActionCellEditor(this));
+        actionColumn.setPreferredWidth(55);
+        //table_.sizeColumnsToFit(0);
+    }
+
+    /**
+     * @return  the players represented by rows in the table
+     */
+    //public abstract OnlineGameTable[] getOnlineGameTables();
+
+    public JTable getTable()
+    {
+        return table_;
+    }
+
+    /**
+     *
+     * @return the table that the player has chosen to sit at if any (at most 1.) return null is not sitting.
+     */
+    public OnlineGameTable getSelectedTable() {
+        return selectedTable_;
+    }
+
+    public void addListSelectionListener(ListSelectionListener l)
+    {
+        table_.getSelectionModel().addListSelectionListener(l);
+    }
+
+    protected PlayerTableModel getModel()
+    {
+        return (PlayerTableModel)table_.getModel();
+    }
+
+    /**
+     * add a row based on a player object
+     * @param onlineTable to add
+     */
+    protected abstract void addRow(OnlineGameTable onlineTable);
+
+
+    /**
+     * clear out all the rows in the table.
+     */
+    public void removeAllRows() {
+
+        PlayerTableModel m = this.getModel();
+        for (int i = m.getRowCount() -1; i >= 0; i--) {
+            m.removeRow(i);
+        }
+    }
+
+    /**
+     *
+     * @param initialPlayerName
+     * @return the new online table to add as a new row.
+     */
+    public abstract OnlineGameTable createOnlineTable(String initialPlayerName);
+
+
+    /**
+     * add another row to the end of the table.
+     */
+    public void addRow(String playersName)
+    {
+        OnlineGameTable onlineTable = createOnlineTable(playersName);
+        addRow(onlineTable);
+    }
+
+
+    protected int getNumColumns() {
+        return COLUMN_NAMES.length;
+    }
+
+    protected static synchronized String getUniqueName() {
+          return "Table "+ counter_++;
+    }
+
+    public void actionPerformed(ActionEvent event) {
+
+        OnlineActionCellRenderer.JoinButton b =
+                (OnlineActionCellRenderer.JoinButton) event.getSource();
+
+        final int joinRow = b.getRow();
+        final PlayerTableModel m = getModel();
+
+        for (int i=0; i<m.getRowCount(); i++) {
+            m.setValueAt(Boolean.valueOf(i != joinRow), i, 0);
+        }
+
+        table_.removeEditor();
+    }
+
+}
