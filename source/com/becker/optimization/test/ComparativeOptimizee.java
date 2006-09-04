364fe63aaf6582b98b06c53a5daeb7cd7398aa31
Barry Becker <barrybecker4@yahoo.com>
2006-09-04 14:03:35 +0000

    cleanup optimization package.

---
+++ source/com/becker/optimization/test/ComparativeOptimizee.java
@@ -0,0 +1,25 @@
+package com.becker.optimization.test;
+
+import com.becker.optimization.*;
+
+/**
+ * Concrete adapter class for a comparison base Optimizee
+ *
+ * @author Barry Becker Date: Aug 20, 2006
+ */
+public abstract class ComparativeOptimizee implements Optimizee {
+
+    public boolean evaluateByComparison() {
+        return true;
+    }
+
+    // not used for ComparativeOptimizee
+    public double evaluateFitness(ParameterArray params) {
+        return 0;
+    }
+
+    public double getOptimalFitness() {
+        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+}
