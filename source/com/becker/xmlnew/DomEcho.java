c747df99971ce6f1ad2ed3861b6609381fb05c24
Barry Becker <barrybecker4@yahoo.com>
2006-10-08 16:33:29 +0000

    mv xml to xml new. Thw will unfortunately lose the history.

---
+++ source/com/becker/xmlnew/DomEcho.java
@@ -0,0 +1,133 @@
+package com.becker.xml;
+
+import org.w3c.dom.*;
+
+import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.TreePath;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+
+
+public class DomEcho  extends JPanel
+{
+    // Global value so it can be ref'd by the tree-adapter
+    static Document document;
+
+    static final int windowHeight = 460;
+    static final int leftWidth = 300;
+    static final int rightWidth = 340;
+    static final int windowWidth = leftWidth + rightWidth;
+
+    public DomEcho()
+    {
+       // Make a nice border
+       EmptyBorder eb = new EmptyBorder(5,5,5,5);
+       BevelBorder bb = new BevelBorder(BevelBorder.LOWERED);
+       CompoundBorder cb = new CompoundBorder(eb,bb);
+       this.setBorder(new CompoundBorder(cb,eb));
+
+       // Set up the tree
+       JTree tree = new JTree(new DomToTreeModelAdapter(document));
+
+       // Iterate over the tree and make nodes visible
+       // (Otherwise, the tree shows up fully collapsed)
+       //TreePath nodePath = ???;
+       //  tree.expandPath(nodePath);
+
+       // Build left-side view
+       JScrollPane treeView = new JScrollPane(tree);
+       treeView.setPreferredSize(
+           new Dimension( leftWidth, windowHeight ));
+
+       // Build right-side view
+       // (must be final to be referenced in inner class)
+       final
+       JEditorPane htmlPane = new JEditorPane();//"text/html","");
+       htmlPane.setEditable(false);
+       JScrollPane htmlView = new JScrollPane(htmlPane);
+       htmlView.setPreferredSize(
+           new Dimension( rightWidth, windowHeight ));
+
+       // Wire the two views together. Use a selection listener
+       // created with an anonymous inner-class adapter.
+       tree.addTreeSelectionListener(
+         new TreeSelectionListener() {
+           public void valueChanged(TreeSelectionEvent e) {
+             TreePath p = e.getNewLeadSelectionPath();
+             if (p != null) {
+                 AdapterNode adpNode =
+                    (AdapterNode) p.getLastPathComponent();
+                 NamedNodeMap attribMap = adpNode.domNode.getAttributes();
+                 String attribs = DomUtil.getAttributeList(attribMap);
+
+                 htmlPane.setText(attribs);
+             }
+           }
+         }
+       );
+
+       // Build split-pane view
+       JSplitPane splitPane =
+          new JSplitPane( JSplitPane.HORIZONTAL_SPLIT,
+                          treeView,
+                          htmlView );
+       splitPane.setContinuousLayout( true );
+       splitPane.setDividerLocation( leftWidth );
+       splitPane.setPreferredSize(
+            new Dimension( windowWidth + 10, windowHeight+10 ));
+
+       // Add GUI components
+       this.setLayout(new BorderLayout());
+       this.add("Center", splitPane );
+    } // constructor
+
+
+    public static void makeFrame() {
+        // Set up a GUI framework
+        JFrame frame = new JFrame("DOM Echo");
+        frame.addWindowListener(
+          new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {System.exit(0);}
+          }
+        );
+
+        // Set up the tree, the views, and display it all
+        final DomEcho echoPanel =
+           new DomEcho();
+        frame.getContentPane().add("Center", echoPanel );
+        frame.pack();
+        Dimension screenSize =
+           Toolkit.getDefaultToolkit().getScreenSize();
+        int w = windowWidth + 10;
+        int h = windowHeight + 10;
+        frame.setLocation(screenSize.width/3 - w/2,
+                          screenSize.height/2 - h/2);
+        frame.setSize(w, h);
+        frame.setVisible(true);
+    } // makeFrame
+
+
+    // -------------------------------------------------------------------------
+    public static void main(String argv[])
+    {
+        if (argv.length != 1) {
+            document = DomUtil.buildDom();
+            makeFrame();
+            return;
+        }
+
+        File file = new File(argv[0]);
+
+        document = DomUtil.parseXMLFile(file);
+
+        makeFrame();
+    } // main
+
+}
---
+++ source/com/becker/xmlnew/DomUtil.java
@@ -0,0 +1,215 @@
+package com.becker.xml;
+
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import java.util.*;
+import java.io.IOException;
+import java.io.File;
+
+/**
+ * User: Barry Becker
+ * Date: Oct 17, 2004
+ */
+public class DomUtil {
+
+    private DomUtil() {};
+
+    /**
+     * Initialize a dom document structure.
+     * @return dom Document
+     */
+    public static Document buildDom()
+    {
+        Document document = null;
+        DocumentBuilderFactory factory =
+           DocumentBuilderFactory.newInstance();
+        try {
+          DocumentBuilder builder = factory.newDocumentBuilder();
+          document = builder.newDocument();  // Create from whole cloth
+
+          Element root = document.createElement("rootElement");
+          document.appendChild(root);
+
+          // normalize text representation
+          // getDocumentElement() returns the document's root node
+          document.getDocumentElement().normalize();
+
+        } catch (ParserConfigurationException pce) {
+            // Parser with specified options can't be built
+            pce.printStackTrace();
+
+        }
+        return document;
+    }
+
+    /**
+     * go through the dom hier and remove spurious text nodes and alse
+     * replace "use" nodes with a deep copy of what they refer to.
+     * @param root
+     * @param document
+     */
+    public static void postProcessDocument(Node root, Document document, boolean replaceUseWithDeepCopy) {
+        NodeList l = root.getChildNodes();
+
+        List deleteList = new ArrayList();
+
+        for (int i=0; i<l.getLength(); i++) {
+            Node n = l.item(i);
+            String name = n.getNodeName();
+            if (name!=null && name.startsWith("#text"))  {
+                // delete if nothing by whitespace
+                String text = n.getNodeValue();
+                if (text.matches("[ \\t\n\\x0B\\f\\r]*")) {
+                    //System.out.println("TEXT="+text+"|so removing");
+                    deleteList.add(n);
+                    //node.removeChild(n);
+                }
+            }
+
+            postProcessDocument(n, document, replaceUseWithDeepCopy);
+
+            if (name!=null && "use".equals(name)) {
+                // substitue the element with the specified id
+                NamedNodeMap attrs = n.getAttributes();
+                Node attr =  attrs.item(0);
+                assert "ref".equals(attr.getNodeName()): "attr name="+attr.getNodeName();
+                String attrValue = attr.getNodeValue();
+
+                System.out.println("searching for "+attrValue);
+                Node element = document.getElementById(attrValue);
+                Node clonedElement = element.cloneNode(replaceUseWithDeepCopy);
+
+                // now we still need to recursively clean the node that was replaced
+                // since it might also contain use nodes.
+                postProcessDocument(clonedElement, document, replaceUseWithDeepCopy);
+
+                root.replaceChild(clonedElement, n);
+            }
+        }
+
+        Iterator it = deleteList.iterator();
+        while (it.hasNext()) {
+            Node n = (Node)it.next();
+            root.removeChild(n);
+        }
+    }
+
+    /**
+     * get the value for an attribute.
+     */
+    public static String getAttribute(Node node, String attribName) {
+        NamedNodeMap attribMap = node.getAttributes();
+        String attributeVal = null;
+        if (attribMap == null)
+            return null;
+        //assert (attribMap!=null) : "no attributes for " +node.getNodeName()+" "+node.getNodeValue();
+
+        for (int i=0; i<attribMap.getLength(); i++) {
+            Node attr = attribMap.item(i);
+            if (attr.getNodeName().equals(attribName))
+                attributeVal = attr.getNodeValue();
+        }
+        assert (attributeVal!=null):
+                "no attribute named '"+attribName+"' for node '"+node.getNodeName()+"' val="+node.getNodeValue();
+        return attributeVal;
+    }
+
+    /**
+     * a concatenated list of the node's attributes.
+     * @param attribMap
+     * @return
+     */
+    public static String getAttributeList(NamedNodeMap attribMap)
+    {
+        String attribs = "";
+        if (attribMap!= null) {
+            attribMap.getLength();
+            //String id = attribMap.
+
+            for (int i=0; i<attribMap.getLength(); i++) {
+                Node n = attribMap.item(i);
+                attribs += n.getNodeName()+"=\""+n.getNodeValue() +"\"  ";
+            }
+        }
+        return attribs;
+    }
+
+    /**
+     * print a text representation of the dom hierarchy.
+     * @param root
+     * @param level
+     */
+    public static void printTree(Node root, int level) {
+         NodeList l = root.getChildNodes();
+         for (int i=0; i<level; i++)
+             System.out.print("    ");
+
+         NamedNodeMap attribMap = root.getAttributes();
+         String attribs = getAttributeList(attribMap);
+
+         System.out.println("Node: <"+root.getNodeName()+">  "+ attribs);
+         for (int i=0; i<l.getLength(); i++) {
+            printTree(l.item(i), level+1);
+         }
+     }
+
+    /*
+    public static Node getRootNode(Document document) {
+        // assume that we have a single root under the document root
+        NodeList children = document.getChildNodes();
+        assert (children.getLength() == 2): "Expected one root instead got "+(children.getLength() - 1);
+        Node root = children.item(1);
+
+        return root;
+    }
+    */
+
+
+    /**
+     * parse an xml file and return a cleaned up Document object.
+     * Set replaceUseWithDeepCopy to false if you are in a debug mode and don't want to see a lot of redundant subtrees.
+     * @param file
+     * @param replaceUseWithDeepCopy if true then replace each instance of a use node with a deep copy of what it refers to
+     * @return the parsed file as a Document
+     */
+    public static Document parseXMLFile(File file, boolean replaceUseWithDeepCopy)
+    {
+        Document document = null;
+        DocumentBuilderFactory factory =
+            DocumentBuilderFactory.newInstance();
+        factory.setValidating(true);
+        //factory.setNamespaceAware(true);
+        try {
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            document = builder.parse( file );
+
+            postProcessDocument(document, document, replaceUseWithDeepCopy);
+            //printTree(document, 0);
+
+        } catch (SAXException sxe) {
+           // Error generated during parsing)
+           Exception  x = sxe;
+           if (sxe.getException() != null)
+               x = sxe.getException();
+           x.printStackTrace();
+
+        } catch (ParserConfigurationException pce) {
+            // Parser with specified options can't be built
+            pce.printStackTrace();
+
+        } catch (IOException ioe) {
+           // I/O error
+           ioe.printStackTrace();
+        }
+        return document;
+    }
+
+    public static Document parseXMLFile(File file)
+    {
+       return parseXMLFile(file, true);
+    }
+}
