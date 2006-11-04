75368a7f74f81ccfc9e9fe8a595dbf19bbf88e17
Barry Becker <barrybecker4@yahoo.com>
2006-11-04 14:51:19 +0000

    minor initial refactoring.

--- source/com/becker/simulation/reactiondiffusion/GrayScott.java
+++ source/com/becker/simulation/reactiondiffusion/GrayScott.java
@@ -1,7 +1,7 @@
 package com.becker.simulation.reactiondiffusion;
 
 /**
- *
+ * This is the core of the Gray-Scott reaction diffusion simulation.
  */
 final class GrayScott {
   double[][] u;
@@ -39,24 +39,27 @@ final class GrayScott {
   public void initialState() {
     uMax = 0.5;
     double v0 = f/(2*(f+k)) + Math.sqrt(f/(2*(f+k))*f/(2*(f+k)) - f*(f+k));
-    double u0 = f/(v0*v0+f);
+    //double u0 = f/(v0*v0+f);
     for (int x = 0; x<width; x++) {
       for (int y = 0; y<height; y++) {
         tmpU[x][y] = 1;
         tmpV[x][y] = 0;
       }
     }
-    for (int x = 0; x<(width/3); x++) {
-      for (int y = 0; y<height/3; y++) {
-        tmpU[1*width/3 + x][1*height/3 + y] = 0.5;
-        tmpV[1*width/3 + x][1*height/3 + y] = 0.25;
+    int w3 = width/3;
+    int h3 = height/3;
+    for (int x = 0; x < w3; x++) {
+      for (int y = 0; y < h3; y++) {
+        tmpU[w3 + x][h3 + y] = 0.5;
+        tmpV[w3 + x][h3 + y] = 0.25;
       }
     }
-
-    for (int x = 0; x<(width/7); x++) {
-      for (int y = 0; y<height/5; y++) {
-        tmpU[5*width/7 + x][3*height/5 + y] = 0.5;
-        tmpV[5*width/7 + x][3*height/5 + y] = 0.25;
+    int w7 = width/7;
+    int h5 = height/5;
+    for (int x = 0; x < w7; x++) {
+      for (int y = 0; y < h5; y++) {
+        tmpU[5 * w7 + x][3 * h5 + y] = 0.5;
+        tmpV[5 * w7 + x][3 * h5 + y] = 0.25;
       }
     }
   }
@@ -71,16 +74,25 @@ final class GrayScott {
 
 
   public void timeStep(double dt) {
-    double newMax = 0;
+
     double uv2;
-    /*centre*/
+    /*center*/
     for (int x = 1; x<width-1; x++) {
       for (int y = 1; y<height-1; y++) {
         uv2 = tmpU[x][y]*tmpV[x][y]*tmpV[x][y];
-        u[x][y] = tmpU[x][y] + dt*(duDivh2*(tmpU[x+1][y] + tmpU[x-1][y] + tmpU[x][y+1] + tmpU[x][y-1] - 4*tmpU[x][y]) - uv2 + f*(1-tmpU[x][y]));
-        if (u[x][y]<0) u[x][y] = 0;
-        v[x][y] = tmpV[x][y] + dt*(dvDivh2*(tmpV[x+1][y] + tmpV[x-1][y] + tmpV[x][y+1] + tmpV[x][y-1] - 4*tmpV[x][y]) + uv2 - k*tmpV[x][y]);
-        if (v[x][y]<0) v[x][y] = 0;
+        double sumU = tmpU[x+1][y] + tmpU[x-1][y] +
+                      tmpU[x][y+1] + tmpU[x][y-1] -
+                      4*tmpU[x][y];
+        u[x][y] = tmpU[x][y] + dt * (duDivh2 * sumU - uv2 + f * (1.0-tmpU[x][y]));
+        if (u[x][y] < 0)
+            u[x][y] = 0;
+
+        double sumV = tmpV[x+1][y] + tmpV[x-1][y] +
+                      tmpV[x][y+1] + tmpV[x][y-1] -
+                      4*tmpV[x][y];
+        v[x][y] = tmpV[x][y] + dt * (dvDivh2 * sumV + uv2 - k * tmpV[x][y]);
+        if (v[x][y] < 0)
+            v[x][y] = 0;
       }
     }
     /*edges*/
@@ -88,30 +100,71 @@ final class GrayScott {
     for (x = 0; x<width; x++) {
       y = 0;
       uv2 = tmpU[x][y]*tmpV[x][y]*tmpV[x][y];
-      u[x][y] = tmpU[x][y] + dt*(duDivh2*(tmpU[pBC(x+1,width)][y] + tmpU[pBC(x-1,width)][y] + tmpU[x][pBC(y+1, height)] + tmpU[x][pBC(y-1, height)] - 4*tmpU[x][y]) - uv2 + f*(1-tmpU[x][y]));
-      if (u[x][y]<0) u[x][y] = 0;
-      v[x][y] = tmpV[x][y] + dt*(dvDivh2*(tmpV[pBC(x+1,width)][y] + tmpV[pBC(x-1,width)][y] + tmpV[x][pBC(y+1, height)] + tmpV[x][pBC(y-1, height)] - 4*tmpV[x][y]) + uv2 - k*tmpV[x][y]);
-      if (v[x][y]<0) v[x][y] = 0;
+      double sumU = tmpU[pBC(x+1,width)][y] + tmpU[pBC(x-1,width)][y] +
+                    tmpU[x][pBC(y+1, height)] + tmpU[x][pBC(y-1, height)] -
+                    4*tmpU[x][y];
+      u[x][y] = tmpU[x][y] + dt * (duDivh2 * sumU - uv2 + f*(1.0-tmpU[x][y]));
+      if (u[x][y]<0)
+          u[x][y] = 0;
+
+      double sumV = tmpV[pBC(x+1,width)][y] + tmpV[pBC(x-1,width)][y] +
+                    tmpV[x][pBC(y+1, height)] + tmpV[x][pBC(y-1, height)] -
+                    4*tmpV[x][y];
+      v[x][y] = tmpV[x][y] + dt * (dvDivh2 * sumV + uv2 - k*tmpV[x][y]);
+      if (v[x][y] < 0)
+          v[x][y] = 0;
+
       y = height - 1;
       uv2 = tmpU[x][y]*tmpV[x][y]*tmpV[x][y];
-      u[x][y] = tmpU[x][y] + dt*(duDivh2*(tmpU[pBC(x+1,width)][y] + tmpU[pBC(x-1,width)][y] + tmpU[x][pBC(y+1, height)] + tmpU[x][pBC(y-1, height)] - 4*tmpU[x][y]) - uv2 + f*(1-tmpU[x][y]));
-      if (u[x][y]<0) u[x][y] = 0;
-      v[x][y] = tmpV[x][y] + dt*(dvDivh2*(tmpV[pBC(x+1,width)][y] + tmpV[pBC(x-1,width)][y] + tmpV[x][pBC(y+1, height)] + tmpV[x][pBC(y-1, height)] - 4*tmpV[x][y]) + uv2 - k*tmpV[x][y]);
-      if (v[x][y]<0) v[x][y] = 0;
+
+      sumU = tmpU[pBC(x+1,width)][y] + tmpU[pBC(x-1,width)][y] +
+             tmpU[x][pBC(y+1, height)] + tmpU[x][pBC(y-1, height)] -
+             4*tmpU[x][y];
+      u[x][y] = tmpU[x][y] + dt * (duDivh2 * sumU - uv2 + f * (1.0-tmpU[x][y]));
+      if (u[x][y] < 0)
+          u[x][y] = 0;
+
+      sumV = tmpV[pBC(x+1,width)][y] + tmpV[pBC(x-1,width)][y] +
+             tmpV[x][pBC(y+1, height)] + tmpV[x][pBC(y-1, height)] -
+              4*tmpV[x][y];
+      v[x][y] = tmpV[x][y] + dt*(dvDivh2 * sumV + uv2 - k*tmpV[x][y]);
+      if (v[x][y] < 0)
+          v[x][y] = 0;
     }
+
     for (y = 0; y<height; y++) {
       x = 0;
       uv2 = tmpU[x][y]*tmpV[x][y]*tmpV[x][y];
-      u[x][y] = tmpU[x][y] + dt*(duDivh2*(tmpU[pBC(x+1,width)][y] + tmpU[pBC(x-1,width)][y] + tmpU[x][pBC(y+1, height)] + tmpU[x][pBC(y-1, height)] - 4*tmpU[x][y]) - uv2 + f*(1-tmpU[x][y]));
-      if (u[x][y]<0) u[x][y] = 0;
-      v[x][y] = tmpV[x][y] + dt*(dvDivh2*(tmpV[pBC(x+1,width)][y] + tmpV[pBC(x-1,width)][y] + tmpV[x][pBC(y+1, height)] + tmpV[x][pBC(y-1, height)] - 4*tmpV[x][y]) + uv2 - k*tmpV[x][y]);
-      if (v[x][y]<0) v[x][y] = 0;
+      double sumU = tmpU[pBC(x+1,width)][y] + tmpU[pBC(x-1,width)][y] +
+                    tmpU[x][pBC(y+1, height)] + tmpU[x][pBC(y-1, height)] -
+                    4*tmpU[x][y];
+      u[x][y] = tmpU[x][y] + dt * (duDivh2 * sumU - uv2 + f*(1.0-tmpU[x][y]));
+      if (u[x][y] < 0)
+          u[x][y] = 0;
+
+      double sumV = tmpV[pBC(x+1,width)][y] + tmpV[pBC(x-1,width)][y] +
+                    tmpV[x][pBC(y+1, height)] + tmpV[x][pBC(y-1, height)] -
+                    4*tmpV[x][y];
+      v[x][y] = tmpV[x][y] + dt * (dvDivh2 * sumV + uv2 - k*tmpV[x][y]);
+      if (v[x][y] < 0)
+          v[x][y] = 0;
+
       x = width - 1;
       uv2 = tmpU[x][y]*tmpV[x][y]*tmpV[x][y];
-      u[x][y] = tmpU[x][y] + dt*(duDivh2*(tmpU[pBC(x+1,width)][y] + tmpU[pBC(x-1,width)][y] + tmpU[x][pBC(y+1, height)] + tmpU[x][pBC(y-1, height)] - 4*tmpU[x][y]) - uv2 + f*(1-tmpU[x][y]));
-      if (u[x][y]<0) u[x][y] = 0;
-      v[x][y] = tmpV[x][y] + dt*(dvDivh2*(tmpV[pBC(x+1,width)][y] + tmpV[pBC(x-1,width)][y] + tmpV[x][pBC(y+1, height)] + tmpV[x][pBC(y-1, height)] - 4*tmpV[x][y]) + uv2 - k*tmpV[x][y]);
-      if (v[x][y]<0) v[x][y] = 0;
+
+      sumU = tmpU[pBC(x+1,width)][y] + tmpU[pBC(x-1,width)][y] +
+             tmpU[x][pBC(y+1, height)] + tmpU[x][pBC(y-1, height)] -
+             4*tmpU[x][y];
+      u[x][y] = tmpU[x][y] + dt * (duDivh2 * sumU - uv2 + f*(1.0-tmpU[x][y]));
+      if (u[x][y] < 0)
+          u[x][y] = 0;
+
+      sumV = tmpV[pBC(x+1,width)][y] + tmpV[pBC(x-1,width)][y] +
+             tmpV[x][pBC(y+1, height)] + tmpV[x][pBC(y-1, height)] -
+             4*tmpV[x][y];
+      v[x][y] = tmpV[x][y] + dt * (dvDivh2 * sumV + uv2 - k*tmpV[x][y]);
+      if (v[x][y] < 0)
+          v[x][y] = 0;
     }
 
     for (x = 0; x<width; x++) {
@@ -121,8 +174,12 @@ final class GrayScott {
       }
     }
   }
+    
 
-  private int pBC(int x, int max) {                        /*periodic boundary conditions*/
+  /**
+   *  Periodic boundary conditions.
+   */
+  private static int pBC(int x, int max) {
     int xp = x;
     while (xp<0) xp += max;
     while (xp>=max) xp -= max;
