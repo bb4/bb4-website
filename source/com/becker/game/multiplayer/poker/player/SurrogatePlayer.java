3d84946331f846198604aae956161d8e49b1d65e
Barry Becker <barrybecker4@yahoo.com>
2007-02-03 18:41:09 +0000

    xml error handler.

---
+++ source/com/becker/game/multiplayer/poker/player/SurrogatePlayer.java
@@ -0,0 +1,28 @@
+package com.becker.game.multiplayer.poker.player;
+
+import com.becker.game.common.online.*;
+
+import java.awt.*;
+
+/**
+ * @author Barry Becker Date: Feb 3, 2007
+ */
+public class SurrogatePlayer extends PokerPlayer implements OnlineChangeListener {
+
+    private ServerConnection connection_;
+
+    public SurrogatePlayer(String name, int money, Color color, boolean isHuman, ServerConnection connection) {
+        super(name, money, color, isHuman);
+        connection_ = connection;
+        connection_.addOnlineChangeListener(this);
+    }
+
+
+    /**
+     * Update ourselves based on
+     * @param cmd
+     */
+    public void handleServerUpdate(GameCommand cmd) {
+      
+    }
+}
