874a7c392dc1a9b807a97dabe0e3d3f85a3b2d5d
BarryBecker4 <BarryBecker4@5fdb6067-0ee0-0310-88fc-90c9ac7ca001>
2010-03-27 17:14:35 +0000

    got negamax with quiescnece working.

---
+++ trunk/source/com/becker/game/twoplayer/tictactoe/test/ExpectedSearchStrategyResults.java
@@ -0,0 +1,126 @@
+package com.becker.game.twoplayer.tictactoe.test;
+
+import com.becker.common.Location;
+import com.becker.game.common.GamePiece;
+import com.becker.game.twoplayer.common.TwoPlayerMove;
+import com.becker.game.twoplayer.common.search.test.strategy.ExpectedMoveMatrix;
+
+/**
+ * Expected generated moves for search tests for both MiniMax and NegaMax algorthms since
+ * the should produce identiacal resutls in all cases (just the implementation is a little different).
+ * @author Barry Becker
+ */
+public class ExpectedSearchStrategyResults {
+
+    private static final GamePiece PLAYER1_PIECE = new GamePiece(true);
+    private static final GamePiece PLAYER2_PIECE = new GamePiece(false);
+
+    private ExpectedSearchStrategyResults() {}
+
+    static final ExpectedMoveMatrix EXPECTED_ZERO_LOOKAHEAD_MOVES = new ExpectedMoveMatrix(
+            TwoPlayerMove.createMove(new Location(2, 2), 16, PLAYER1_PIECE),  // beginningP1
+            TwoPlayerMove.createMove(new Location(1, 1), -8, PLAYER2_PIECE),  // beginningP2
+            TwoPlayerMove.createMove(new Location(1, 2), 88, PLAYER1_PIECE),  // middleP1
+            TwoPlayerMove.createMove(new Location(1, 3), -88, PLAYER2_PIECE), // middleP2
+            TwoPlayerMove.createMove(new Location(3, 1), 0, PLAYER1_PIECE),   // endP1
+            TwoPlayerMove.createMove(new Location(2, 3), -48, PLAYER2_PIECE)  // endP2
+    );
+
+    static final ExpectedMoveMatrix EXPECTED_ONE_LEVEL_LOOKAHEAD = new ExpectedMoveMatrix(
+            TwoPlayerMove.createMove(new Location(1, 2), 4, PLAYER2_PIECE),
+            TwoPlayerMove.createMove(new Location(2, 1), 48, PLAYER1_PIECE),
+            TwoPlayerMove.createMove(new Location(3, 2), 28, PLAYER2_PIECE),
+            TwoPlayerMove.createMove(new Location(3, 2), 0, PLAYER1_PIECE),  //   "lateMidGameO";   seems wrong. should be  (1,2)
+            TwoPlayerMove.createMove(new Location(2, 1), 0, PLAYER2_PIECE),
+            TwoPlayerMove.createMove(new Location(3, 1), -12, PLAYER1_PIECE)   // seems ok
+    );
+
+    static final ExpectedMoveMatrix EXPECTED_ONE_LEVEL_WITH_QUIESCENCE = new ExpectedMoveMatrix(
+
+            TwoPlayerMove.createMove(new Location(1, 2), 4, PLAYER2_PIECE),  // beginningP1
+            TwoPlayerMove.createMove(new Location(3, 3), -4, PLAYER1_PIECE), //             "midGameCenterO"
+            TwoPlayerMove.createMove(new Location(3, 2), 28, PLAYER2_PIECE), // middleP1    "lateMidGameX" :
+            TwoPlayerMove.createMove(new Location(1, 2), 0, PLAYER1_PIECE),//             "lateMidGameO";   
+            TwoPlayerMove.createMove(new Location(2, 1), 0, PLAYER2_PIECE),  // endP1       "endGameX";
+            TwoPlayerMove.createMove(new Location(3, 3), -52, PLAYER1_PIECE) // endP2       "endGame0";
+            /*
+            TwoPlayerMove.createMove(new Location(1, 2), 4, PLAYER2_PIECE),  // beginningP1
+            TwoPlayerMove.createMove(new Location(2, 1), 48, PLAYER1_PIECE), //             "midGameCenterO"   // exp 3,3   X
+            TwoPlayerMove.createMove(new Location(3, 2), 28, PLAYER2_PIECE), // middleP1    "lateMidGameX" :
+            TwoPlayerMove.createMove(new Location(1, 2), 0, PLAYER1_PIECE),  //             "lateMidGameO";
+            TwoPlayerMove.createMove(new Location(2, 1), 0, PLAYER2_PIECE),  // endP1       "endGameX";
+            TwoPlayerMove.createMove(new Location(3, 3), -52, PLAYER1_PIECE)); // endP2       "endGame0";
+            */
+    );
+
+    static final ExpectedMoveMatrix EXPECTED_TWO_LEVEL_WITH_QUIESCENCE = new ExpectedMoveMatrix(
+        TwoPlayerMove.createMove(new Location(1, 2), 4, new GamePiece(false)),
+        TwoPlayerMove.createMove(new Location(2, 1), 48, new GamePiece(true)),
+        TwoPlayerMove.createMove(new Location(3, 2), 28, new GamePiece(false)),
+        TwoPlayerMove.createMove(new Location(1, 2), 0, new GamePiece(true)),
+        TwoPlayerMove.createMove(new Location(2, 1), 0, new GamePiece(false)),
+        TwoPlayerMove.createMove(new Location(3, 3), -52, new GamePiece(true))
+    );
+
+    static final ExpectedMoveMatrix EXPECTED_THREE_LEVEL_WITH_QUIESCENCE = new ExpectedMoveMatrix(
+
+        TwoPlayerMove.createMove(new Location(1, 2), 4, new GamePiece(false)),
+        TwoPlayerMove.createMove(new Location(2, 1), 48, new GamePiece(true)),
+        TwoPlayerMove.createMove(new Location(3, 2), 28, new GamePiece(false)),
+        TwoPlayerMove.createMove(new Location(1, 2), 0, new GamePiece(true)),
+        TwoPlayerMove.createMove(new Location(2, 1), 0, new GamePiece(false)),
+        TwoPlayerMove.createMove(new Location(3, 1), -12, new GamePiece(true))
+    );
+
+    static final ExpectedMoveMatrix EXPECTED_TWO_LEVEL_LOOKAHEAD = new ExpectedMoveMatrix(
+            TwoPlayerMove.createMove(new Location(1, 2), 4, PLAYER2_PIECE),
+            TwoPlayerMove.createMove(new Location(2, 1), 48, PLAYER1_PIECE),
+            TwoPlayerMove.createMove(new Location(3, 2), 28, PLAYER2_PIECE),
+            TwoPlayerMove.createMove(new Location(1, 2), 0, PLAYER1_PIECE),
+            TwoPlayerMove.createMove(new Location(2, 1), 0, PLAYER2_PIECE),
+            TwoPlayerMove.createMove(new Location(3, 3), -52, PLAYER1_PIECE)
+    );
+
+    static final ExpectedMoveMatrix EXPECTED_FOUR_LEVEL_LOOKAHEAD = new ExpectedMoveMatrix(
+            TwoPlayerMove.createMove(new Location(1, 1), 8, PLAYER2_PIECE),
+            TwoPlayerMove.createMove(new Location(2, 1), 48, PLAYER1_PIECE),
+            TwoPlayerMove.createMove(new Location(3, 2), 28, PLAYER2_PIECE),
+            TwoPlayerMove.createMove(new Location(1, 2), 0, PLAYER1_PIECE),
+            TwoPlayerMove.createMove(new Location(2, 1), 0, PLAYER2_PIECE),
+            TwoPlayerMove.createMove(new Location(3, 1), -12, PLAYER1_PIECE)
+    );
+
+    static final ExpectedMoveMatrix EXPECTED_FOUR_LEVEL_BEST_20_PERCENT = new ExpectedMoveMatrix(
+            TwoPlayerMove.createMove(new Location(1, 2), 4, PLAYER2_PIECE),
+            TwoPlayerMove.createMove(new Location(2, 1), 48, PLAYER1_PIECE),
+            TwoPlayerMove.createMove(new Location(3, 2), 28, PLAYER2_PIECE),
+            TwoPlayerMove.createMove(new Location(1, 2), 0, PLAYER1_PIECE),
+            TwoPlayerMove.createMove(new Location(2, 1), 0, PLAYER2_PIECE),
+            TwoPlayerMove.createMove(new Location(3, 1), -12, PLAYER1_PIECE)
+    );
+
+    static final ExpectedMoveMatrix EXPECTED_FOUR_LEVEL_WITH_QUIESCENCE = new ExpectedMoveMatrix(
+            TwoPlayerMove.createMove(new Location(1, 2), 4, PLAYER2_PIECE),  // beginningP1
+            TwoPlayerMove.createMove(new Location(2, 1), 48, PLAYER1_PIECE), //             "midGameCenterO"
+            TwoPlayerMove.createMove(new Location(3, 2), 28, PLAYER2_PIECE), // middleP1    "lateMidGameX"
+            TwoPlayerMove.createMove(new Location(1, 2), 0, PLAYER1_PIECE),  //             "lateMidGameO"
+            TwoPlayerMove.createMove(new Location(2, 1), 0, PLAYER2_PIECE),  // endP1       "endGameX"
+            TwoPlayerMove.createMove(new Location(3, 1), -12, PLAYER1_PIECE) // endP2       "endGame0"
+             /*
+            TwoPlayerMove.createMove(new Location(1, 2), 4, PLAYER2_PIECE),  // beginningP1
+            TwoPlayerMove.createMove(new Location(3, 3), -4, PLAYER1_PIECE), //             "midGameCenterO"
+            TwoPlayerMove.createMove(new Location(3, 2), 28, PLAYER2_PIECE), // middleP1    "lateMidGameX" :
+            TwoPlayerMove.createMove(new Location(1, 2), 0, PLAYER1_PIECE),  //            "lateMidGameO";
+            TwoPlayerMove.createMove(new Location(2, 1), 0, PLAYER2_PIECE),  // endP1       "endGameX";
+            TwoPlayerMove.createMove(new Location(3, 3), -52, PLAYER1_PIECE) // endP2       "endGame0";    */
+    );
+
+    static final ExpectedMoveMatrix EXPECTED_FOUR_LEVEL_NO_ALPHA_BETA = new ExpectedMoveMatrix(
+            TwoPlayerMove.createMove(new Location(1, 1), 8, PLAYER2_PIECE),
+            TwoPlayerMove.createMove(new Location(2, 1), 48, PLAYER1_PIECE),
+            TwoPlayerMove.createMove(new Location(3, 2), 28, PLAYER2_PIECE),
+            TwoPlayerMove.createMove(new Location(1, 2), 0, PLAYER1_PIECE),
+            TwoPlayerMove.createMove(new Location(2, 1), 0, PLAYER2_PIECE),
+            TwoPlayerMove.createMove(new Location(3, 1), -12, PLAYER1_PIECE)
+    );
+}
