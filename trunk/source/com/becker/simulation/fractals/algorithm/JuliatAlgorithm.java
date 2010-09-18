16487f66319fb78c6528d2d17f0a2f402f1f425c
BarryBecker4 <BarryBecker4@5fdb6067-0ee0-0310-88fc-90c9ac7ca001>
2010-09-18 17:13:32 +0000

    Get mandelbrot/fractal explorer simulation working. Added concurrency as well.

---
+++ trunk/source/com/becker/simulation/fractals/algorithm/JuliatAlgorithm.java
@@ -0,0 +1,32 @@
+package com.becker.simulation.fractals.algorithm;
+
+import com.becker.common.math.ComplexNumber;
+
+/**
+ * Populates the FractalModel using the iterative Mandelbrot algorithm1..
+ *
+ * @author Barry Becker
+ */
+public class JuliatAlgorithm extends FractalAlgorithm  {
+
+    public static final int MAX_ITERATIONS = 500;
+
+    public JuliatAlgorithm(FractalModel model) {
+        super(model, new ComplexNumber(-2.1, -1.5), new ComplexNumber(1.1, 1.5));
+        model.setCurrentRow(0);
+    }
+
+    @Override
+    public int getFractalValue(ComplexNumber initialValue) {
+
+        ComplexNumber z = initialValue;
+        int numIterations = 0;
+
+        while (z.getMagnitude() < 2.0 && numIterations <  MAX_ITERATIONS) {
+            z = z.power(2).add(initialValue);
+            numIterations++;
+        }
+        return numIterations;
+    }
+    
+}
