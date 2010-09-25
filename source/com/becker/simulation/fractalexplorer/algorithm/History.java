b0705ae5ffeaa2eed1b5c45dd28fe5aee97f7126
Barry Becker <barrybecker4@yahoo.com>
2010-09-25 16:54:28 +0000

    trying to add back fractal explorer.

---
+++ source/com/becker/simulation/fractalexplorer/algorithm/History.java
@@ -0,0 +1,36 @@
+package com.becker.simulation.fractalexplorer.algorithm;
+
+import com.becker.common.math.ComplexNumberRange;
+
+import java.util.LinkedList;
+
+/**
+ * Maintains regions (ranges) in the complex space that were visited, so
+ * we can back up.
+ * @author Barry Becker
+ */
+public class History {
+
+
+    /** range of bounding box in complex plane. */
+    private LinkedList<ComplexNumberRange> stack;
+
+
+    public History() {
+        stack = new LinkedList<ComplexNumberRange>();
+    }
+
+
+    public void addRangeToHistory(ComplexNumberRange range)  {
+        stack.push(range);
+    }
+
+    /**
+     * Back up one step in the history
+     * @return
+     */
+    public ComplexNumberRange popLastRange() {
+         return stack.pop();
+    }
+
+}
