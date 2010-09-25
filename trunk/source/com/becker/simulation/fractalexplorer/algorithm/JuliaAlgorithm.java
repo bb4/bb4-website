530bc363990fff4867949eba804b8c7f45832ff7
BarryBecker4 <BarryBecker4@5fdb6067-0ee0-0310-88fc-90c9ac7ca001>
2010-09-25 16:54:28 +0000

    trying to add back fractal explorer.

---
+++ trunk/source/com/becker/simulation/fractalexplorer/algorithm/JuliaAlgorithm.java
@@ -0,0 +1,34 @@
+package com.becker.simulation.fractalexplorer.algorithm;
+
+import com.becker.common.math.ComplexNumber;
+import com.becker.common.math.ComplexNumberRange;
+
+/**
+ * Populates the FractalModel using the iterative Julia set algorithm.
+ *
+ * @author Barry Becker
+ */
+public class JuliaAlgorithm extends FractalAlgorithm  {
+
+    private static final ComplexNumberRange INITIAL_RANGE =
+            new ComplexNumberRange(new ComplexNumber(-1.8, -1.7), new ComplexNumber(1.8, 1.7));
+
+    public JuliaAlgorithm(FractalModel model) {
+        super(model, INITIAL_RANGE);
+        model.setCurrentRow(0);
+    }
+
+    @Override
+    public double getFractalValue(ComplexNumber initialValue) {
+
+        ComplexNumber z = initialValue;
+        int numIterations = 0;
+
+        while (z.getMagnitude() < 2.0 && numIterations < getMaxIterations()) {
+            z = z.power(2).add(initialValue);
+            numIterations++;
+        }
+        return (double) numIterations / getMaxIterations();
+    }
+    
+}
