af98f1fdeb230d784c3834810510618fbc40d233
Barry Becker <barrybecker4@yahoo.com>
2011-02-26 19:36:02 +0000

    Add sierpinski in new location.

---
+++ source/com/becker/apps/sierpinski/SierpinskiApplet.java
@@ -0,0 +1,57 @@
+package com.becker.apps.sierpinski;
+
+import com.becker.ui.GUIUtil;
+import com.becker.ui.components.ResizableAppletPanel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SierpinskiApplet extends JApplet {
+
+    ResizableAppletPanel resizablePanel_ = null;
+
+    /**
+     * Constructor
+     */
+    public SierpinskiApplet() {
+        commonInit();
+    }
+
+    private void commonInit() {
+        GUIUtil.setCustomLookAndFeel();
+
+        enableEvents( AWTEvent.WINDOW_EVENT_MASK );
+        setFont( new Font( "Serif", Font.PLAIN, 14 ) );
+
+        JPanel mainPanel = createMainPanel();
+
+        resizablePanel_ = new ResizableAppletPanel( mainPanel );
+        this.getContentPane().add( resizablePanel_ );
+    }
+
+    private JPanel createMainPanel() {
+        SierpinskiComponent sierpinskiComp = new SierpinskiComponent();
+        sierpinskiComp.setBorder(BorderFactory.createEtchedBorder());
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout( new BorderLayout() );
+        mainPanel.add(sierpinskiComp, BorderLayout.CENTER );
+
+        return mainPanel;
+    }
+
+    /**
+     * This method allow javascript to resize the applet from the browser.
+     */
+    @Override
+    public void setSize( int width, int height )  {
+        resizablePanel_.setSize( width, height );
+    }
+
+    //------ Main method --------------------------------------------------------
+
+    public static void main( String[] args ) {
+        SierpinskiApplet simulator = new SierpinskiApplet();
+        GUIUtil.showApplet( simulator, "Sierpinski Triangle" );
+    }
+}
---
+++ source/com/becker/apps/sierpinski/SierpinskiPanel.java
@@ -0,0 +1,32 @@
+package com.becker.apps.sierpinski;
+
+import com.becker.common.math.function.Function;
+import com.becker.simulation.graphing.FunctionType;
+import com.becker.ui.components.NumberInput;
+import com.becker.ui.renderers.FunctionRenderer;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Draws a recursive Sierpinksi triangle.
+ * @author Barry Becker
+ */
+public class SierpinskiPanel extends JPanel {
+
+    SierpinskiRenderer renderer;
+
+    public SierpinskiPanel() {
+        renderer = new SierpinskiRenderer();
+    }
+
+    public void setRecursiveDepth(int depth) {
+        renderer.setDepth(depth);
+    }
+
+    @Override
+    public void paint( Graphics g ) {
+        renderer.setSize(getWidth(), getHeight());
+        renderer.paint(g);
+    }
+}
