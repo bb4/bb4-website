4e2fb4a1a70fa1441bb6786919e0d4d0290bb16e
Barry Becker <barrybecker4@yahoo.com>
2011-03-13 00:22:05 +0000

    prepare for upload to website.

---
+++ source/com/becker/apps/sp/SierpinskiApplet.java
@@ -0,0 +1,45 @@
+package com.becker.apps.sierpinski;
+
+import com.becker.ui.components.ResizableAppletPanel;
+import com.becker.ui.util.GUIUtil;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SierpinskiApplet extends JApplet {
+
+    ResizableAppletPanel resizablePanel_ = null;
+
+    /**
+     * Constructor
+     */
+    public SierpinskiApplet() {
+        commonInit();
+    }
+
+    private void commonInit() {
+        GUIUtil.setCustomLookAndFeel();
+        JPanel mainPanel = createMainPanel();
+
+        resizablePanel_ = new ResizableAppletPanel( mainPanel );
+        this.getContentPane().add( resizablePanel_ );
+    }
+
+    private JPanel createMainPanel() {
+        SierpinskiComponent sierpinskiComp = new SierpinskiComponent();
+        sierpinskiComp.setBorder(BorderFactory.createEtchedBorder());
+
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout( new BorderLayout() );
+        mainPanel.add(sierpinskiComp, BorderLayout.CENTER );
+
+        return mainPanel;
+    }
+
+    //------ Main method --------------------------------------------------------
+
+    public static void main( String[] args ) {
+        SierpinskiApplet simulator = new SierpinskiApplet();
+        GUIUtil.showApplet( simulator, "Sierpinski Triangle" );
+    }
+}
