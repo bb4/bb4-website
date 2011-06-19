77e63d99ec092b098991a337867386f919a60294
Barry Becker <barrybecker4@yahoo.com>
2011-06-19 00:30:33 +0000

    Remove cached group analysis information from GoGroup. Moved to GroupAnalyzer. Added map of analyzers for all go groups.

--- source/com/becker/game/twoplayer/go/board/analysis/eye/information/EyeInformation.java
+++ source/com/becker/game/twoplayer/go/board/analysis/eye/information/EyeInformation.java
@@ -12,7 +12,7 @@ import com.becker.game.twoplayer.go.board.elements.eye.IGoEye;
 public interface EyeInformation
 {
     /**
-     * The life property should be regarded as a property slightly below Bensons definition
+     * The life property should be regarded as a property slightly below Benson's definition
      * of unconditional life, because if we have an AliveInAtari status for an eye, it might be
      * necessary to play inside the eye, but with the great advantage that detecting it
      * is just a matter of counting neighbours as it will be shown in Section 4.
