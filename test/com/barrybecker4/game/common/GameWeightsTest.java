347d648cd9e3d8b7e6a50dcd66634087a97289f2
Barry Becker <barrybecker4@yahoo.com>
2012-07-14 23:02:50 +0000

    Renamed package becker to barrybecker4. Massive amount of changes, but all fairly mechanical. Some manual updates in distribute.xml were needed. Follow up to last commit because it was too big to do at once.

---
+++ test/com/barrybecker4/game/common/GameWeightsTest.java
@@ -0,0 +1,45 @@
+// Copyright by Barry G. Becker, 2012. Licensed under MIT License: http://www.opensource.org/licenses/MIT
+package com.barrybecker4.game.common;
+
+import com.barrybecker4.game.twoplayer.pente.pattern.SimpleWeights;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Verify that we correctly evaluate patterns on the board.
+ *
+ * @author Barry Becker
+ */
+public class GameWeightsTest extends TestCase  {
+
+    /** instance under test */
+    private GameWeights weights;
+
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        weights = new SimpleWeights();
+    }
+
+    public void testConstruction() {
+        assertEquals("unexpected name ",
+                "Weight 1", weights.getName(1));
+        assertEquals("unexpected description ",
+                "The weighting coefficient for the 1th term of the evaluation polynomial", weights.getDescription(1));
+        assertEquals("unexpected param 2 value ",
+                10.0, weights.getPlayer1Weights().get(2).getValue());
+    }
+
+    /**
+     * @return the line
+     */
+    private StringBuilder createLine(String line) {
+        return new StringBuilder(line);
+    }
+
+    public static Test suite() {
+        return new TestSuite(GameWeightsTest.class);
+    }
+}
---
+++ test/com/barrybecker4/game/twoplayer/common/search/examples/MoveCreator.java
@@ -0,0 +1,53 @@
+/** Copyright by Barry G. Becker, 2000-2011. Licensed under MIT License: http://www.opensource.org/licenses/MIT  */
+package com.barrybecker4.game.twoplayer.common.search.examples;
+
+import com.barrybecker4.common.geometry.Location;
+import com.barrybecker4.game.twoplayer.common.search.TwoPlayerMoveStub;
+
+/**
+ * Create stub two player moves
+ * Factory.
+ *
+ * @author Barry Becker
+ */
+public class MoveCreator {
+
+    private static final int FAKE_BOARD_SIZE = 19;
+
+    EvaluationPerspective evalPerspective;
+
+    int moveCount = 0;
+
+    public MoveCreator(EvaluationPerspective evalPersp) {
+        evalPerspective = evalPersp;
+    }
+
+    public TwoPlayerMoveStub createMove(int value, boolean player1Move, TwoPlayerMoveStub parent) {
+
+        return new TwoPlayerMoveStub(value, player1Move, createToLocation(), parent);
+    }
+
+    /**
+     * The location is not really useed, just give it something unique so the hash works.
+     * @return
+     */
+    private Location createToLocation() {
+        moveCount++;
+        return new Location(moveCount / FAKE_BOARD_SIZE, moveCount % FAKE_BOARD_SIZE);
+    }
+
+    /**
+     * We can tell our depth in the tree by looking at the number of ancestors we have.
+     * @param parent
+     * @return depth in game tree.
+     */
+    private int getDepth(TwoPlayerMoveStub parent) {
+        int depth = 0;
+        TwoPlayerMoveStub nextParent = parent;
+        while (nextParent != null) {
+            nextParent = nextParent.getParent();
+            depth++;
+        }
+        return depth;
+    }
+}
