f76e8c1f04096f84f46914a9bfa707c779dd9b37
Barry Becker <barrybecker4@yahoo.com>
2013-03-31 15:27:38 +0000

    more java2d cleanup

--- main/source/com/barrybecker4/java2d/ui/RightSizer.java
+++ main/source/com/barrybecker4/java2d/ui/RightSizer.java
@@ -9,33 +9,31 @@ import java.net.URL;
 /**
  * Shows an image in a frame of the same size.
  */
-public class RightSizer extends ApplicationFrame
-{
-    private Image mImage;
+public class RightSizer extends ApplicationFrame {
 
-    public static void main( String[] args ) throws Exception
-    {
-        String url = "http://barrybecker4.com/family_portrait_s.JPG";
-        if ( args.length > 0 )
+    private Image image;
+
+    public static void main( String[] args ) throws Exception {
+        String url = "http://barrybecker4.com/familyPictures/family_portrait_s.JPG";
+        if ( args.length > 0 ) {
             url = args[0];
+        }
 
         new RightSizer( new URL( url ) );
     }
 
-    public RightSizer( URL url )
-    {
+    public RightSizer( URL url ) {
         super( "RightSizer v1.0" );
-        mImage = Toolkit.getDefaultToolkit().getImage( url );
+        image = Toolkit.getDefaultToolkit().getImage( url );
         rightSize();
     }
 
     /**
      * Set the frame size to the same size as the image (once it has loaded).
      */
-    private void rightSize()
-    {
-        int width = mImage.getWidth( this );
-        int height = mImage.getHeight( this );
+    private void rightSize() {
+        int width = image.getWidth( this );
+        int height = image.getHeight( this );
         if ( width == -1 || height == -1 ) return;
         addNotify();
         Insets insets = getInsets();
@@ -46,9 +44,8 @@ public class RightSizer extends ApplicationFrame
     }
 
     @Override
-    public boolean imageUpdate( Image img, int infoflags,
-                                int x, int y, int width, int height )
-    {
+    public boolean imageUpdate(Image img, int infoflags,
+                               int x, int y, int width, int height) {
         if ( (infoflags & ImageObserver.ERROR) != 0 ) {
             System.out.println( "Error loading image!" );
             System.exit( -1 );
@@ -67,15 +64,13 @@ public class RightSizer extends ApplicationFrame
     }
 
     @Override
-    public void update( Graphics g )
-    {
+    public void update( Graphics g ) {
         paint( g );
     }
 
     @Override
-    public void paint( Graphics g )
-    {
+    public void paint( Graphics g ) {
         Insets insets = getInsets();
-        g.drawImage( mImage, insets.left, insets.top, this );
+        g.drawImage(image, insets.left, insets.top, this );
     }
 }
