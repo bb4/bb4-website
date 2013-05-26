002f5e34393844d3948d504fb2cfae605b367469
BarryBecker4 <BarryBecker4@5fdb6067-0ee0-0310-88fc-90c9ac7ca001>
2013-05-26 15:09:57 +0000

    i18n fixes.

--- trunk/imageproc/source/com/barrybecker4/java2d/examples/AllFonts.java
+++ trunk/imageproc/source/com/barrybecker4/java2d/examples/AllFonts.java
@@ -3,14 +3,46 @@ package com.barrybecker4.java2d.examples;
 import com.barrybecker4.ui.application.ApplicationFrame;
 
 import javax.swing.*;
-import java.awt.*;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GraphicsEnvironment;
+import java.awt.RenderingHints;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Derived from code accompanying "Java 2D Graphics" by Jonathan Knudsen.
+ * It's interesting to note that very few fonts support all unicode characters
+ * (and if they do, they probably take up a lot of memory).
+ * About 10% support japanese characters and about 10% suport vietnamese characters for example, but
+ * not the same 10%. The fonts that I found that support german, japanese, and vietnamese characters are:
+ *  *Dialog
+ *   DialogInput
+ *   Serif      (font have a small line trailing from the edges of letters and symbols)
+ *  *SansSerif  (font without the small trailing lines)
+ *
+ *  In my opinion, of these, SansSerif or Dialog look the best.
  */
 public class AllFonts {
 
     private static final int ROW_HEIGHT = 30;
+    private static final int FONT_SIZE = 20;
+
+
+    private static final String ENGLISH = "The quick brown fox jumped over the lazy dog.";
+    private static final String JAPANESE = "\u56f2\u7881\u30b2\u30fc\u30e0\u306e\u60c5\u5831";
+    private static final String GERMAN = "ungefähr Ausführungen";
+    private static final String VIETNAMESE = "m\u00F4 ph\u1ECFng";
+
+    private static final String SPECIAL_CHARS = "\u56f2\u7881äü\u00F4\u1ECF";
+
+    private static final String SEPARATOR = "  |  ";
+    private static final String MULTI_LINGUAL_STRING =
+            ENGLISH + SEPARATOR + JAPANESE + SEPARATOR + GERMAN + SEPARATOR + VIETNAMESE;
+
 
     private static void showAllFonts() {
         Font[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAllFonts();
@@ -52,23 +84,53 @@ public class AllFonts {
               drawAllFonts(g2);
          }
 
-         private void drawAllFonts(Graphics2D g2) {
+        private void drawAllFonts(Graphics2D g2) {
 
-             String[] fonts =
-                    GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames(); //getAllFonts();
-             for ( int i = 0; i < fonts.length; i++ ) {
-                 Font font = new Font(fonts[i], Font.PLAIN, 24 );
-                 drawFontMessage(i, font, g2);
-             }
-         }
+            List<Font> fonts = getAllFonts();
+            int i=0;
+
+            for (Font font : fonts) {
+                drawFontMessage(i++, font, g2);
+            }
+        }
+
+        /** get a list of all fonts, with the ones that support japanese and vietnamese characters first */
+        private List<Font> getAllFonts() {
+            List<Font> multiLingualFonts = new ArrayList<Font>();
+            List<Font> otherFonts = new ArrayList<Font>();
+
+            String[] allFonts =
+                    GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+            for (String fontName : allFonts) {
+                Font font = new Font(fontName, Font.PLAIN, FONT_SIZE);
+                if (supportsChars(font, SPECIAL_CHARS)) {
+                    multiLingualFonts.add(font);
+                } else {
+                    otherFonts.add(font);
+                }
+            }
+            List<Font> combinedList = new ArrayList<Font>(multiLingualFonts);
+            combinedList.addAll(otherFonts);
+            return combinedList;
+        }
+
+        private boolean supportsChars(Font font, String specialChars){
+
+            for (int i=0; i< specialChars.length(); i++) {
+                if (!font.canDisplay(specialChars.charAt(i))) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
 
         private void drawFontMessage(int i, Font font, Graphics2D g2)  {
-            //Font font = new Font( "Serif", Font.PLAIN, 24 );
 
             g2.setFont( font );
-            int yPos = 10 + ROW_HEIGHT * i;
+            int yPos = 25 + ROW_HEIGHT * i;
             g2.drawString(font.getName() , 40, yPos );
-            g2.drawString("The quick brown fox jumped over the lazy dog.", 500, yPos );
+            g2.drawString(MULTI_LINGUAL_STRING , 500, yPos);
         }
     }
 
