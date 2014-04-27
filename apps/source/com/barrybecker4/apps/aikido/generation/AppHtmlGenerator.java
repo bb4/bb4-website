09a3c7c81ff1533ccd420b1f03b4815bd293573d
Barry Becker <barrybecker4@yahoo.com>
2014-04-27 21:16:42 +0000

    All technique configuration.

--- apps/source/com/barrybecker4/apps/aikido/generation/AppHtmlGenerator.java
+++ apps/source/com/barrybecker4/apps/aikido/generation/AppHtmlGenerator.java
@@ -1,5 +1,5 @@
 // Copyright by Barry G. Becker, 2014. Licensed under MIT License: http://www.opensource.org/licenses/MIT
-package com.barrybecker4.apps.aikido;
+package com.barrybecker4.apps.aikido.generation;
 
 import com.barrybecker4.common.util.FileUtil;
 import org.w3c.dom.Document;
@@ -13,7 +13,8 @@ import java.io.IOException;
 public class AppHtmlGenerator {
 
     /** location where data files are read from */
-    static final String PROJECT_DIR = FileUtil.getHomeDir() + "apps/source/com/barrybecker4/apps/aikido/";
+    public static final String PROJECT_DIR =
+            FileUtil.getHomeDir() + "apps/source/com/barrybecker4/apps/aikido/generation/";
 
     private static final String JAVASCRIPT_FILE = PROJECT_DIR + "methods.js";
     private static final String BODY_HTML_FILE = PROJECT_DIR + "body.html";
